ac11ab01b483572549efd718dba36e75f1867213
Roaa Alhakimi <58307502+ro5a@users.noreply.github.com>
2022-02-21 21:18:30 +0300

    edit code and add user

--- app.js
+++ app.js
@@ -1,53 +1,51 @@
-var express=require("express");
-var bodyParser=require("body-parser");
-
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/gfg');
-var db=mongoose.connection;
-db.on('error', console.log.bind(console, "connection error"));
-db.once('open', function(callback){
-	console.log("connection succeeded");
-})
-
-var app=express()
-app.use(express.static(__dirname + "/../public"));
-
-
-
-app.use(bodyParser.json());
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
-
-app.post('/sign_up', function(req,res){
-	var name = req.body.name;
-	var email =req.body.email;
-	var pass = req.body.password;
-	var file =req.body.file;
-
-	var data = {
-		"name": name,
-		"email":email,
-		"password":pass,
-		"file":file
-	}
-db.collection('details').insertOne(data,function(err, collection){
-		if (err) throw err;
-		console.log("Record inserted Successfully");
-			
-	});
-		
-	return res.redirect('signup_success.html');
-})
-
-
-app.get('/',function(req,res){
-res.set({
-	'Access-control-Allow-Origin': '*'
-	});
-return res.redirect('index.html');
-}).listen(3000)
-
-
-console.log("server listening at port 3000");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var flash = require('express-flash');
+var session = require('express-session');
+var usersRouter = require('./routes/users');
+ 
+ 
+var app = express();
+ 
+ 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+ 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+ 
+app.use(session({ 
+    secret: '123456catr',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+}))
+ 
+app.use(flash());
+app.use('/', usersRouter);
+ 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+ 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+ 
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+ 
+module.exports = app;

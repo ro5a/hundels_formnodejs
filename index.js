585654b661d9479349f35f9e2d58f61cfa9d4701
Roaa Alhakimi <58307502+ro5a@users.noreply.github.com>
2022-02-26 14:11:32 +0300

    form image

--- index.js
+++ index.js
@@ -1,40 +1,53 @@
 const express=require("express");
 const multer=require('multer');
 const mongoose=require("mongoose");
-const { getMaxListeners } = require("./models/user");
+
 const User=require('./models/user');
+
 const app=express();
+const storage=multer.diskStorage({destination:(req,file,cb)=>{
+    if(file.mimetype=="image/png"|| file.mimetype== "image/jpg")
+    cb(null,'public/images');
+    else if(file.mimetype=="application/pdf ")
+    cb(null,"public/pdf/");
+},filename:(req,file,cb)=>{
+    var extension=file.originalname.split(".");
+    var ext= extension[extension.length=1];
+    var uploaded_file_name=file.fieldname+"-"+Date.now() + '-' + Math.round(Math.random() * 1E9)+"."+ext;
+    cb(null, uploaded_file_name);
+}});
 const upload=multer({
-    dest:'public/uplodads',
+    storage:storage,
     fileFilter:(req,file,callback)=>{
-        if(file.mimetype=="image/png"){
+        if(file.mimetype=="image/png"|| file.mimetype== "image/jpg"|| file.mimetype=="application/pdf" ){
             callback(null,true)
         }
-    }
+    },
+    limits:1024*1024*5 
 });
 app.set('view engine',"ejs");
 app.use(express.urlencoded());
  mongoose.connect("mongodb://localhost:27017/hundle-form")
 .then((result)=>{
     
-    console.log(result);
+    // console.log(result);
 })
 .catch((error)=>{
 console.log(error);
-});
+}); 
 
 app.get('/home',auth,(req,res)=>{
     res.render('home'); 
 });
-app.post('/add_user',upload.single('image ') ,(req,res)=>{
-    const s= new User({
-        id:mongoose.Types.ObjectId,
-        name:req.body.username,
-        username:req.body.name,
-        email:req.body.email
-    });
-    s.save();
-console.log(req.body);
+app.post('/add_user',upload.single('u_image') ,(req,res)=>{
+    // const s= new User({
+    //     id:mongoose.Types.ObjectId,
+    //     name:req.body.username, 
+    //     username:req.body.name,
+    //     email:req.body.email
+    // });
+//     s.save();
+// console.log(req.body);
 res.end();
 });
 function auth(req,res,next){
--- models/user.js
+++ models/user.js
@@ -2,6 +2,9 @@ const mongoose=require('mongoose');
 const Schema=mongoose.Schema;
 
 const user_schema=new Schema({
+   id:{type:String,
+   required:true
+    },
      name:{type:String,
            required:true,
            unique:true
